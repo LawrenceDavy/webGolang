2b261a10b0f757b34695a167c7b33a80665cf1a4
LawrenceDavy13 <LawrenceDavy13@gmail.com>
2019-07-01 21:43:20 +0100

    planning stage

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 var tpl *template.Template
 
 type pageData struct {
 	Title string
-	FirstName string
+	Date string
+	Score int
+	Descripton string
 }
+
+
 
 func init() {
 	tpl = template.Must(template.ParseGlob("template/*.html"))
@@ -20,8 +25,7 @@ func init() {
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/about", about)
-	http.HandleFunc("/contact", contact)
-	http.HandleFunc("/apply", apply)
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -41,35 +45,6 @@ func about(w http.ResponseWriter, r *http.Request) {
 		Title: "About Page",
 	}
 	err := tpl.ExecuteTemplate(w, "about.html", pd)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-	}
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	pd := pageData {
-		Title: "Contact Page",
-	}
-	err := tpl.ExecuteTemplate(w, "contact.html", pd)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-	}
-}
-
-func apply(w http.ResponseWriter, r *http.Request) {
-
-	pd := pageData {
-		Title: "Apply Page",
-	}
-
-	var first string
-	if r.Method == http.MethodPost {
-		first = r.FormValue("fname")
-		pd.FirstName = first
-	}
-	err := tpl.ExecuteTemplate(w, "apply.html", pd)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
